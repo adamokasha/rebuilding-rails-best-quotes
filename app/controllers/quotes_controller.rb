520658991e473deae2c0c82862d2f333b417aded
Adam Okasha <adam.okasha@shopify.com>
2023-02-08 18:13:13 -0500

    Chapter 5 - Basic Models
    Add updating

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,3 +1,5 @@
+require "securerandom"
+
 class QuotesController < Rulers::Controller
   def a_quote
     @adjective = "cool"
@@ -5,7 +7,40 @@ class QuotesController < Rulers::Controller
     render :a_quote, { adjective: @adjective, ruby_version: @ruby_version }
   end
 
+  def index
+    quotes = FileModel.all
+    render :index, :quotes => quotes
+  end
+
+  def quote_1
+    quote_1 = FileModel.find(1)
+    render :quote, :obj => quote_1
+  end
+
+  def update
+    raise "Not found" unless env["REQUEST_METHOD"].downcase == "post"
+
+    quote = FileModel.find(1)
+    quote.update({
+      "submitter" => SecureRandom.uuid
+    })
+    quote.save
+
+    render :quote, :obj => quote
+  end
+
   def exception
     raise "It's an exception!"
+  end
+
+  def new_quote
+    attrs = {
+      "submitter" => "web user",
+      "quote" => "A picture is work one k pixels",
+      "attribution" => "Me"
+    }
+
+    quote = FileModel.create(attrs)
+    render :quote, :obj => quote
   end
 end
